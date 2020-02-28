3d6ad1ea3b6025caf0e2792752063380815e0229
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2020-02-28 17:41:34 +0100

    Create a clothing application with create-react-app

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Directory from '../../components/directory/directory.component';
+// import App from "./App";
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+
+export default HomePage;
