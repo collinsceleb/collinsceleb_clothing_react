304692b5781484974b3da6c36398be944689913b
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2020-03-19 14:21:35 +0100

    Added persist to the application, also updated cart.selectors.js

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -22,12 +22,10 @@ export const selectCartItemsCount = createSelector(
     )
 );
 
-export const selectCartTotal = createSelector(
-  [selectCartItems],
-  cartItems =>
-    cartItems.reduce(
-      (accumulatedQuantity, cartItem) =>
-        accumulatedQuantity + cartItem.quantity * cartItem.price,
-      0
-    )
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce(
+    (accumulatedQuantity, cartItem) =>
+      accumulatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
 );
